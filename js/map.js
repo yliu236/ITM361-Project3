48e634f115dce645ef0d0998da05f05e730b69d8
yliu236 <46697061+yliu236@users.noreply.github.com>
2019-11-13 01:16:17 -0600

    put back what I had before for the map.js

--- js/map.js
+++ js/map.js
@@ -1,9 +1,16 @@
-
+var marker;
 function initMap() { 
     var map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 41.881, lng: -87.623}, 
         zoom: 13    
     });
     
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 41.8663, lng: -87.6068}
+    });
+    marker.addListner('click', toggleBounce);
 }
 
