b19eed04f7419dbbf018fc064f1af9500a01e75d
yliu236 <46697061+yliu236@users.noreply.github.com>
2019-11-26 11:55:03 -0600

    Fixing the map issue

--- js/map.js
+++ js/map.js
@@ -1,18 +1,15 @@
 function initMap() {
-    var fp = {lat: 41.881, lng: -87.623};
-    var map = new google.maps.Map(document.getElementById('map'), {center: fp, zoom: 14});
+  var fp = {lat: 41.881, lng: -87.623};
 
-    var infowindow = new google.maps.InfoWindow({
-        content: 'Shedd Aquarium'
-    });
+  var map = new google.maps.Map(
+    document.getElementById('map'), {
+    zoom: 12,
+    center: fp});
 
-    var marker = new google.maps.Marker({
-        position: fp,
-        map: map,
-        icon: 'images/location.jpg'
-    });
-    
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
+  var marker = new google.maps.Marker({
+    position: fp,
+    map: map
+  });
 }
+
+window.addEventListener('load', initMap );
