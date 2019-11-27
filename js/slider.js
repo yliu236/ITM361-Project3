6d4b0ab6bb2e79ae1d11123fd4b197eb5a29bfc4
yliu236 <46697061+yliu236@users.noreply.github.com>
2019-11-26 22:57:49 -0600

    put the js sider bar inside page

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+    auto: true,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 800
+    });
+  });
